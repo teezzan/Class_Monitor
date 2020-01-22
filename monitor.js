4dea7ee6771021aa46af4f1687d65da2c42ea041
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-01-22 02:05:02 +0100

    changed fixed port issues

--- monitor.js
+++ monitor.js
@@ -119,4 +119,4 @@ app.get("/api/images", function (req, res) {
     });
 
 const port = process.env.PORT || 3000;
-app.listen(3000, ()=> console.log(`Listening on port ${port}`))
+app.listen(port, ()=> console.log(`Listening on port ${port}`))
